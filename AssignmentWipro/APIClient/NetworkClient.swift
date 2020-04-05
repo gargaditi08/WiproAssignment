83097ff0c5c2cd21b914aa38e9c1533f5f18851e
Aditi Garg <aditigarg@Aditis-MacBook-Air.local>
2020-04-05 16:30:57 +0530

    Network class

---
+++ AssignmentWipro/APIClient/NetworkClient.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkClient.swift
+//  AssignmentWipro
+//
+//  Created by Aditi Garg on 05/04/20.
+//  Copyright © 2020 Aditi Garg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public typealias NetworkRouterCompletionHandler = (_ data: Data?,_ response: URLResponse?, _ error: Error?)->()
+
+
+internal final class NetworkClient
+{
+    private var task : URLSessionTask?
+    
+    func loadRequest(_ request : URLRequest, completion: @escaping NetworkRouterCompletionHandler) {
+        
+        let session = URLSession.shared
+        do {
+            task = session.dataTask(with: request, completionHandler: { data, response, error in
+                DispatchQueue.main.async {
+                    completion(data, response, error)
+                }
+            
+        })
+    }
+        self.task?.resume()
+}
+
+    func cancel(){
+        self.task?.cancel()
+}
+}
---
+++ AssignmentWipro/Extension/AsyncImageView.swift
@@ -0,0 +1,60 @@
+//
+//  AsyncImageView.swift
+//  AssignmentWipro
+//
+//  Created by Aditi Garg on 05/04/20.
+//  Copyright © 2020 Aditi Garg. All rights reserved.
+//
+
+import UIKit
+
+//Marck: -  'asyncImageCacheArray' is a global variable cached UIImage
+var asyncImageCacheArray = NSCache<NSString, UIImage>()
+
+class AsyncImageView: UIImageView {
+    
+    //Marck : Variables
+    private var currentURL : NSString?
+    
+    //Mark: - Public Methods
+    
+    func loadAsyncFrom(url : String, placeholder : UIImage?) {
+        let imageURL = url as NSString
+        
+        //To check is image is already in cache then load it
+        
+        if let cachedImage = asyncImageCacheArray.object(forKey: imageURL) {
+            image = cachedImage
+            return
+        }
+        image = placeholder
+        currentURL = imageURL
+        guard let requestURL = URL(string: url) else { image = placeholder; return}
+        URLSession.shared.dataTask(with: requestURL) { (data, response, error) in
+            DispatchQueue.main.async { [weak self] in
+                if error == nil {
+                    
+                    if let imageData = data {
+                        if self?.currentURL == imageURL {
+                           
+                            if let imageToPresent = UIImage (data: imageData){
+                              asyncImageCacheArray.setObject(imageToPresent, forKey: imageURL)
+                                self?.image = imageToPresent
+                            } else {
+                                self?.image = placeholder
+                            }
+                        }
+                    } else {
+                        self?.image = placeholder
+                    }
+                } else {
+               self?.image = placeholder
+            }
+        }
+        
+    }.resume()
+    
+    
+    }
+    
+}
